8d8036febf4dfefe9d4c55a22cf9390b5fa4f0ab
Rushil Kumar <darksentinal13@gmail.com>
2023-08-26 12:10:10 +0000

    commit

---
+++ frequencyAnalysis.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+
+void findFrequency(int arr[], int n) {
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] == -1) continue;  
+
+        int freq = 1;  
+
+        
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[j] == arr[i]) {
+                freq++;
+                arr[j] = -1;  
+            }
+        }
+
+        cout << arr[i] << ": " << freq << " times\n";
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    cout << "Frequency of elements in the array:\n";
+    findFrequency(arr, n);
+
+    return 0;
+}
