fe73e4ea658f0e266cf88b2f31dfcb5b53e07f76
Rushil Kumar <darksentinal13@gmail.com>
2023-09-28 08:44:40 +0000

    commit

--- ReverseWords_babbar.cpp
+++ ReverseWords_babbar.cpp
@@ -1,11 +1,23 @@
 #include<iostream>
 using namespace std;
+void rev(string str){
+    int start = 0;
+    int end = str.length()-1;
+    while(start<end){
+        swap(str[start],str[end]);
+    }
+}
 string Reverse(string s){
-    for(int i=0;i<s.length();i++){
-        
-    }   
+    string temp = "";
+    for(int i=0;i<s.length();i++){//traverse the string
+    string temp1 = s[i];
+        if(s[i]==' ' || s[i]=='\0'){
+            rev(temp);
+        }
+    }
+    
 }
 int main(){
-    string s = "My name is Rushil Kumar";
-    cout<<s;
+    string lol = "My name is Rushil Kumar";
+    Reverse(lol)
 }
